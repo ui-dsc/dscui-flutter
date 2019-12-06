d7ce31944cabde518b18a267dac6ecc305914762
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2019-12-06 09:49:20 +0700

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -6,60 +6,6 @@ void main() => runApp(MaterialApp(
       home: MyApp(),
     ));
 
-class RandomWord extends StatefulWidget {
-  RandomWord({Key key}) : super(key: key);
-
-  @override
-  _RandomWordState createState() => _RandomWordState();
-}
-
-class _RandomWordState extends State<RandomWord> {
-  String wordpair = WordPair.random(maxSyllables: 2).asPascalCase;
-
-  List<WordPair> _suggestions = <WordPair>[];
-
-  Widget _buildSuggestion() {
-    return ListView.builder(
-      padding: EdgeInsets.all(10.0),
-      itemBuilder: (BuildContext _context, int i) {
-        if (i.isOdd) {
-          return Divider();
-        }
-
-        int index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(prefix0.generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair wp) {
-    return Center(
-      child: ListTile(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Text(
-              wp.asPascalCase,
-            ),
-            Icon(
-              Icons.favorite_border,
-              color: Colors.red,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildSuggestion();
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
@@ -70,7 +16,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Startup Name Generator'),
         ),
-        body: RandomWord(),
+        body: Center(
+          child: Text("HEllo World"),
+        ),
       ),
     );
   }
